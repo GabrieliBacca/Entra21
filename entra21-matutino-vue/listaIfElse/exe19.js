6440afeba716cf7be3ea1e6d3040f55392c1954e
GabrieliBacca <110993557+GabrieliBacca@users.noreply.github.com>
2023-08-28 09:05:57 -0300

    .

--- entra21-matutino-vue/listaIfElse/exe19.js
+++ entra21-matutino-vue/listaIfElse/exe19.js
@@ -1,14 +1,13 @@
+const pacote = require('prompt-sync')
+const prompt = pacote()
 
-/*
 let numero = Number(prompt("Digite um numero: ")) 
 let unidade = numero % 10
 
-    numero = (numero - unidade)/10
+numero = (numero - unidade)/10
+dezena = numero % 10
 
-    dezena = numero % 10
-    numero = (numero - dezena)/10
-    centena = numero
+numero = (numero - dezena)/10
+centena = numero
 
-    dezena = int(dezena)
-    centena = int(centena)
-    print(centena,'centena(s),',dezena,'dezena(s) e',unidade,'unidade(s)')*/
+console.log(centena,'centena(s),',dezena,'dezena(s) e',unidade,'unidade(s)')
