f52abe35c77a868665dd35947e2963c0fb55a035
GabrieliBacca <110993557+GabrieliBacca@users.noreply.github.com>
2023-09-11 08:39:15 -0300

    .

---
+++ entra21-matutino-vue/listaWhile/exe8.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+let num = [];
+
+for (let i = 0; i < 5; i++) {
+    num[i] = Number(prompt(`Digite o ${i + 1}º número: `));
+}
+
+let soma = 0;
+for (let i = 0; i < num.length; i++) {
+    soma += num[i];
+}
+
+const media = soma / num.length;
+
+console.log(`A média é: ${media}`);
--- entra21-matutino-vue/listaWhile/exe9.js
+++ entra21-matutino-vue/listaWhile/exe9.js
@@ -7,14 +7,14 @@ for (let i = 1; i <= 50; i++) {
     num.push(i)
     if(i % 2 !== 0){
         impar.push(i)
-        
     }
     if( i % 2 ==0){
         par.push(i)
     }
 }
-let inverse = impar.slice().reverse()
-console.log(`Numeros impares em ordem decrescente: ${inverse}`)
 
 console.log(`Numeros impares: ${impar}`)
 console.log(`Numeros pares: ${par}`)
+
+
+
