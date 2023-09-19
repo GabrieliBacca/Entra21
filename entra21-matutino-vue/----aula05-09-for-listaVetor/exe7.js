64780942aa90f053795e67e7de5855526abc28f5
GabrieliBacca <baccagabrieli@gmail.com>
2023-09-19 16:09:04 -0300

    .

--- entra21-matutino-vue/----aula05-09-for-listaVetor/exe7.js
+++ entra21-matutino-vue/----aula05-09-for-listaVetor/exe7.js
@@ -1,7 +1,20 @@
+const prompt = require('prompt-sync')();
 
-let num = [2,6,3]
-let num2= [2,6,3]
+let num1 = [2, 3, 5];
+let num2 = [2, 3, 6];
 
-for (let i = 0; i num.length; i++) {
-    
+function vetoresIguais(vetor1, vetor2) {
+    for (let i = 0; i < vetor1.length; i++) {
+        if (vetor1[i] !== vetor2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+if (vetoresIguais(num1, num2)) {
+    console.log("Os vetores são iguais.");
+} else {
+    console.log("Os vetores são diferentes.");
 }
