742f3e335bbab6ff7cf6d64110bd9457e593e188
GabrieliBacca <baccagabrieli@gmail.com>
2023-10-09 10:45:09 -0300

    .

--- Listas-JavaScript-Logica/aula05-09-for-listaVetor/exe1-5.js
+++ Listas-JavaScript-Logica/aula05-09-for-listaVetor/exe1-5.js
@@ -1,3 +1,6 @@
+/*1.5 Faça um programa para cadastrar 10 nomes. Permita o usuário pesquisar um nome através do índice. 
+Ex: O Usuário digita o número 2, e programa mostra o nome cadastrado no índice 2 do vetor.*/
+
 prompt = require('prompt-sync') ()
 
 let nomes = []
