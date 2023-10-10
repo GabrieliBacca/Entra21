56ea77a53755478b3b32c90b256149f04c0675ae
GabrieliBacca <baccagabrieli@gmail.com>
2023-10-10 15:30:19 -0300

    .

--- Projetos/aula10-10/equacao-segundo-grau/main.js
+++ Projetos/aula10-10/equacao-segundo-grau/main.js
@@ -4,7 +4,11 @@ function calcular() {
     const c = Number(document.getElementById("c").value)
 
     const delta = b * b - (4 * a * c)
+    const x1 = (-b + Math.sqrt(delta)) / (2 * a)
+    const x2 = (-b - Math.sqrt(delta)) / (2 * a)
 
     document.getElementById("delta").innerText += delta
+    document.getElementById("x1").innerText += x1
+    document.getElementById("x2").innerText += x2
 }
 
